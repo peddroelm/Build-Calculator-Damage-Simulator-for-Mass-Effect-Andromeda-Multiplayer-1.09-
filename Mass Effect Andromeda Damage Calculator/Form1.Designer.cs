97e6f27911ae5c0aa44645e50aa3369e55891d17
PM <PM@PMMOBILESUS>
2017-07-20 22:08:03 +0300

    autoscroll = on

--- Mass Effect Andromeda Damage Calculator/Form1.Designer.cs
+++ Mass Effect Andromeda Damage Calculator/Form1.Designer.cs
@@ -1631,6 +1631,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1851, 961);
             this.Controls.Add(this.comboBoxBonusPRS);
             this.Controls.Add(this.comboBoxBonusShields);
